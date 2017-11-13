ee8a35992962a9d8c6392ab5e8bd2d9853aaaf36
Dennis Remme <dennis@remme.hamburg>
2017-11-13 12:27:06 +0100

    Improved progress indicator pane a little.

---
+++ src/main/kotlin/hamburg/remme/tinygit/gui/ProgressPane.kt
@@ -0,0 +1,28 @@
+package hamburg.remme.tinygit.gui
+
+import javafx.geometry.Pos
+import javafx.scene.Node
+import javafx.scene.control.ProgressBar
+import javafx.scene.layout.StackPane
+
+class ProgressPane(vararg node: Node) : StackPane(*node) {
+
+    private val progress = ProgressBar(-1.0)
+
+    init {
+        progress.styleClass += "log-progress"
+        progress.maxWidth = Double.MAX_VALUE
+        StackPane.setAlignment(progress, Pos.TOP_CENTER)
+
+        children += progress
+    }
+
+    fun showProgress() {
+        progress.isVisible = true
+    }
+
+    fun hideProgress() {
+        progress.isVisible = false
+    }
+
+}
