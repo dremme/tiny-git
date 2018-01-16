3ebf22486a702b022d592a3985087102ecee9012
Dennis Remme <dennis@remme.hamburg>
2018-01-16 14:46:19 +0100

    Properly set proxy settings in local git config.
    Some CSS stuff.

--- src/main/kotlin/hamburg/remme/tinygit/git/GitConfiguration.kt
+++ src/main/kotlin/hamburg/remme/tinygit/git/GitConfiguration.kt
@@ -3,9 +3,12 @@ package hamburg.remme.tinygit.git
 import hamburg.remme.tinygit.domain.Repository
 
 private val proxy = arrayOf("config", "http.proxy")
-//private val unsetProxy = arrayOf("config", "--unset", "http.proxy")
+private val unsetProxy = arrayOf("config", "--unset", "http.proxy")
 
 fun gitSetProxy(repository: Repository) {
     if (repository.proxyHost.isNotBlank()) git(repository, *proxy, "${repository.proxyHost}:${repository.proxyPort}")
-//    else git(repository, *unsetProxy)
+}
+
+fun gitUnsetProxy(repository: Repository) {
+    git(repository, *unsetProxy)
 }
