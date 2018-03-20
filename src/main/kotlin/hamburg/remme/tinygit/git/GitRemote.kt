ad96662aaa06e5e72a4e2289771cf330dd0277e9
Dennis Remme <dennis@remme.hamburg>
2018-03-20 19:03:48 +0100

    Fixed an issue with renamed branches not being able to be pushed to origin.

--- src/main/kotlin/hamburg/remme/tinygit/git/GitRemote.kt
+++ src/main/kotlin/hamburg/remme/tinygit/git/GitRemote.kt
@@ -45,6 +45,9 @@ fun gitPush(repository: Repository, force: Boolean) {
     if (response.contains("$fatalSeparator.*no upstream branch".toRegex(setOf(IC, G)))) {
         val name = "${fatalSeparator}The current branch (.+) has no upstream branch\\..*".toRegex(setOf(IC, G)).matchEntire(response)!!.groupValues[1]
         response = git(repository, *if (force) pushForce else push, *upstream, name).trim()
+    } else if (response.contains("$fatalSeparator.*does not match.*the name of your current branch".toRegex(setOf(IC, G)))) {
+        val head = gitHead(repository)
+        response = git(repository, *if (force) pushForce else push, *upstream, head.name).trim()
     }
     if (response.contains("$errorSeparator.*tip of your current branch is behind".toRegex(setOf(IC, G)))) throw BranchBehindException()
     else if (response.contains("$fatalSeparator.*timed out".toRegex(setOf(IC, G)))) throw TimeoutException()
