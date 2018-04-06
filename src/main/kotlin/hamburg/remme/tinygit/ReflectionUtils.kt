b9a2a0bce9572ecdd319a46fca283a5d487c27cc
Dennis Remme <dennis@remme.hamburg>
2018-04-06 13:05:31 +0200

    Removed Kotlin reflection API. Java is sufficient for this task.

--- src/main/kotlin/hamburg/remme/tinygit/ReflectionUtils.kt
+++ src/main/kotlin/hamburg/remme/tinygit/ReflectionUtils.kt
@@ -2,8 +2,6 @@ package hamburg.remme.tinygit
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner
 import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.jvmErasure
 
 /**
  * Finds all [KClass]es annotated with the given [Annotation].
@@ -24,9 +22,9 @@ inline fun <reified T : Annotation> findAll(): List<KClass<*>> {
 fun List<KClass<*>>.createSingletonMap(): Map<KClass<*>, Any> {
     val dependencyMap = mutableMapOf<KClass<*>, Any>()
     sortedByDependencies().forEach {
-        val constructor = it.primaryConstructor!!
-        val arguments = constructor.parameters.map { dependencyMap[it.type.jvmErasure] }
-        dependencyMap += it to constructor.call(*arguments.toTypedArray())
+        val constructor = it.java.constructors[0]
+        val arguments = constructor.parameters.map { dependencyMap[it.type.kotlin] }
+        dependencyMap += it to constructor.newInstance(*arguments.toTypedArray())
     }
     return dependencyMap
 }
@@ -35,8 +33,8 @@ fun List<KClass<*>>.createSingletonMap(): Map<KClass<*>, Any> {
  * Sorts the list in order of the classes dependency tree.
  */
 private fun List<KClass<*>>.sortedByDependencies() = sortedWith(Comparator { s1, s2 ->
-    val param1 = s1.primaryConstructor!!.parameters
-    val param2 = s2.primaryConstructor!!.parameters
+    val param1 = s1.java.constructors[0].parameters
+    val param2 = s2.java.constructors[0].parameters
     when {
     // Both constructors have 0 parameters
         param1.isEmpty() && param2.isEmpty() -> 0
@@ -44,11 +42,11 @@ private fun List<KClass<*>>.sortedByDependencies() = sortedWith(Comparator { s1,
         param1.isEmpty() && param2.isNotEmpty() -> -1
         param1.isNotEmpty() && param2.isEmpty() -> 1
     // Both classes depend on each other -> cyclic dependency
-        param1.any { it.type.jvmErasure == s2 } && param2.any { it.type.jvmErasure == s1 }
+        param1.any { it.type.kotlin == s2 } && param2.any { it.type.kotlin == s1 }
         -> throw RuntimeException("Cyclic dependency between $s1 and $s2 detected.")
     // One class has the other one as dependency
-        param2.any { it.type.jvmErasure == s1 } -> -1
-        param1.any { it.type.jvmErasure == s2 } -> 1
+        param2.any { it.type.kotlin == s1 } -> -1
+        param1.any { it.type.kotlin == s2 } -> 1
     // No dependencies between the two
         else -> 0
     }
