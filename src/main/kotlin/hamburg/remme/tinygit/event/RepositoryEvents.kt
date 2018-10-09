a2473aeb6c46a6dccc3c0b3cd89fc6fbff4e2941
Dennis Remme <dennis@remme.hamburg>
2018-10-09 16:43:04 +0200

    Migrated to Java 11 and OpenJFX 11.

--- src/main/kotlin/hamburg/remme/tinygit/event/RepositoryEvents.kt
+++ src/main/kotlin/hamburg/remme/tinygit/event/RepositoryEvents.kt
@@ -15,10 +15,12 @@ class RepositoryClosedEvent : Event()
 
 /**
  * When a Git repository should be updated.
+ * @property directory the path to the Git repository.
  */
 class RepositoryUpdateRequestedEvent(val directory: File) : Event()
 
 /**
  * When a Git repository has been updated (fetch & pull).
+ * @property directory the path to the Git repository.
  */
 class RepositoryUpdatedEvent(val directory: File) : Event()
